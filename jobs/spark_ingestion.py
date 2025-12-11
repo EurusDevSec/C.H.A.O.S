123c29349165d79b13b7b389fdb4e2077a277356
LeganSar44 <hoangslevan@gmail.com>
2025-12-11 15:19:53 +0700

    feat: Add Spark streaming job to ingest Kafka weather data into Delta Lake on MinIO.

--- jobs/spark_ingestion.py
+++ jobs/spark_ingestion.py
@@ -10,4 +10,52 @@ KAFKA_BOOTSTRAP_SERVERS = "yagi_kafka:9092" # Port Internal
 TOPIC = "weather-stream"
 
 
-def 
+def main():
+    # 1. Khởi tạo Spark Session
+    spark = SparkSession.builder \
+        .appName("YagiStormIngestion") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.hadoop.fs.s3a.endpoint", MINIO_ENDPOINT) \
+        .config("spark.hadoop.fs.s3a.access.key", MINIO_ACCESS_KEY) \
+        .config("spark.hadoop.fs.s3a.secret.key", MINIO_SECRET_KEY) \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    #2. Định nghĩa Schema
+    schema = StructType([
+        StructField("datetime", StringType(), True),
+        StructField("temp_c", DoubleType(), True),
+        StructField("wind_kph", DoubleType(), True),
+        StructField("pressure_mb", DoubleType(), True),
+        StructField("precip_mm", DoubleType(), True),
+        StructField("humidity", DoubleType(), True),
+        StructField("cloud", DoubleType(), True),
+        StructField("condition_text", StringType(), True)
+    ])
+
+    # 3. Đọc dữ liệu từ Kafka
+    kafka_df = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS) \
+        .option("subscribe", TOPIC) \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    # 4. Parse JSON
+    parsed_df = kafka_df.select(
+        from_json(col("value").cast("string"), schema).alias("data")
+    ).select("data.*")
+
+    # 5. Ghi dữ liệu xuống MinIO (Delta Lake)
+    query = parsed_df.writeStream \
+        .format("delta") \
+        .option("path", "s3a://yagi/weather") \
+        .option("checkpointLocation", "s3a://yagi/checkpoint") \
+        .start()
+
+    query.awaitTermination()
